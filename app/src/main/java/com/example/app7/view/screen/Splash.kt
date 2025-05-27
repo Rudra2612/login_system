e98ef21f45f52ed04a374a958dcc883621480bd2
01 <rudranavadiya31@gmail.com>
2025-05-27 11:52:45 +0530

    first project

--- app/src/main/java/com/example/app7/view/screen/Splash.kt
+++ app/src/main/java/com/example/app7/view/screen/Splash.kt
@@ -1,16 +1,19 @@
 package com.example.app7.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.app7.R
 import com.example.app7.view.navigation.ScreenRoute
 import kotlinx.coroutines.delay
 
@@ -30,6 +33,6 @@ fun Splash(controller: NavHostController) {
             .background(Color.DarkGray),
         contentAlignment = Alignment.Center
     ) {
-        Text("Splash Screen", fontSize = 40.sp)
+        Image(painter = painterResource(R.drawable.logo), contentDescription = null, modifier = Modifier.size(80.dp))
     }
 }
