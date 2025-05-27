114667563adfc3a34687328c44de13e6dc5e906e
01 <rudranavadiya31@gmail.com>
2025-05-27 11:57:42 +0530

    first project

--- app/src/main/java/com/example/app7/view/screen/Home.kt
+++ app/src/main/java/com/example/app7/view/screen/Home.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +28,9 @@ fun Home(controller: NavHostController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            TextField(value = "hello", onValueChange = {
+
+            })
             Text("Home Screen", fontSize = 40.sp)
             ElevatedButton(onClick = {
                 controller.navigate(
--- app/src/main/java/com/example/app7/view/screen/Splash.kt
+++ app/src/main/java/com/example/app7/view/screen/Splash.kt
@@ -1,19 +1,16 @@
 package com.example.app7.view.screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.app7.R
 import com.example.app7.view.navigation.ScreenRoute
 import kotlinx.coroutines.delay
 
@@ -33,6 +30,6 @@ fun Splash(controller: NavHostController) {
             .background(Color.DarkGray),
         contentAlignment = Alignment.Center
     ) {
-        Image(painter = painterResource(R.drawable.logo), contentDescription = null, modifier = Modifier.size(80.dp))
+        Text("Splash Screen", fontSize = 40.sp)
     }
 }
