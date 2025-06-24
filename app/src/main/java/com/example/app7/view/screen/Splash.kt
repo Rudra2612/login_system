562a595b725fea695ca539bb2a81010cfcd9c939
01 <rudranavadiya31@gmail.com>
2025-06-24 21:44:27 +0530

    first project

--- app/src/main/java/com/example/app7/view/screen/Splash.kt
+++ app/src/main/java/com/example/app7/view/screen/Splash.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
 import com.example.app7.R
 import com.example.app7.controller.PrefController
 import com.example.app7.view.navigation.ScreenRoute
